ea413715a3179c10c15faa76b89d4c605f9ed6da
Javier Martinez Verneri <jamartinezverneri@gmail.com>
2022-12-18 21:28:53 -0300

    unit tests

--- src/test/java/com/javi/moby/CandidateRepositoryTest.java
+++ src/test/java/com/javi/moby/CandidateRepositoryTest.java
@@ -2,6 +2,7 @@ package com.javi.moby;
 
 import com.javi.moby.entity.Candidate;
 import com.javi.moby.repository.ICandidateRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -12,5 +13,20 @@ public class CandidateRepositoryTest {
     @Test
     public void saveCandidateTest(){
         Candidate candidate = new Candidate();
+
+        candidate.setFirstName("Javier");
+        candidate.setSurname("Martinez");
+        candidate.setIdentityType("DNI");
+        candidate.setIdNumber("32444777");
+
+        iCandidateRepository.save(candidate);
+
+        Assertions.assertThat(candidate.getIdCandidate()).isGreaterThan(0);
+    }
+
+    public void getCandidateTest(){
+        Candidate candidate = iCandidateRepository.findById(1L).get();
+
+        Assertions.assertThat(candidate.getIdCandidate()).isEqualTo(1L);
     }
 }
